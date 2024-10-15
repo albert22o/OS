735acb077fb5f8b80404f5b8d4516562a501a936
danyowoj <latsuk1803@gmail.com>
2024-10-15 12:11:21 +0700

    Added lab 4

--- 4/main.c
+++ 4/main.c
@@ -3,63 +3,52 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
-#define MAX_PROCESSES 5
+#define NUM_PROC 3
 
-// Функция для вывода дерева процессов
-void print_process_tree(int level, pid_t pid, const char *name)
+int main()
 {
-    for (int i = 0; i < level; i++)
+    pid_t pids[NUM_PROC];                                                   // Массив для хранения идентификаторов процессов
+    char *programs[NUM_PROC] = {"/bin/ls", "/bin/date", "/usr/bin/whoami"}; // Программы для запуска
+
+    printf("Parent process: PID = %d\n", getppid());
+    printf("Executing programs...\n\n");
+    for (int i = 0; i < NUM_PROC; i++)
     {
-        printf("  "); // Отступ для визуализации уровня
-    }
-    printf("Process ID: %d, Name: %s\n", pid, name);
-}
+        pids[i] = fork(); // Создаем новый процесс
 
-// Главная функция
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
-        fprintf(stderr, "Использование: %s <путь к программе1> <путь к программе2> ...\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    pid_t pids[MAX_PROCESSES];
-
-    // Запуск процессов
-    for (int i = 1; i < argc && i <= MAX_PROCESSES; i++)
-    {
-        pid_t pid = fork(); // Создаем новый процесс
-
-        if (pid < 0)
+        if (pids[i] < 0)
         {
-            perror("Ошибка fork");
+            perror("fork failed"); // Проверяем на ошибки
             exit(EXIT_FAILURE);
         }
-        else if (pid == 0)
-        { // Дочерний процесс
-            // Запускаем программу
-            execvp(argv[i], &argv[i]); // execvp хэндлит аргументы
-            perror("Ошибка execvp");   // Если execvp не удается
-            printf("\n");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {                      // Родительский процесс
-            pids[i - 1] = pid; // Сохраняем идентификатор нового процесса
+
+        if (pids[i] == 0)
+        { // В дочернем процессе
+            // Выполняем программу
+            execlp(programs[i], programs[i], NULL);
+            perror("execlp failed"); // Проверяем на ошибки execlp
+            exit(EXIT_FAILURE);      // Завершаем в случае ошибки
         }
     }
 
-    // Ждем завершения процессов и выводим дерево процессов
-    printf("Дерево процессов:\n");
-    for (int i = 0; i < MAX_PROCESSES && pids[i] > 0; i++)
+    // В родительском процессе
+    for (int i = 0; i < NUM_PROC; i++)
     {
         int status;
-        waitpid(pids[i], &status, 0);                // Ждем завершения дочернего процесса
-        print_process_tree(1, pids[i], argv[i + 1]); // Печатаем дерево
+        waitpid(pids[i], &status, 0); // Ждем завершения дочернего процесса
+        if (WIFEXITED(status))
+        {
+            printf("\nChild process %d: PID = %d, PPID = %d\n", i, getpid(), getppid());
+            printf("finished with exit status %d\n", WEXITSTATUS(status));
+        }
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
+
+
+    [Родительский процесс]
+         /        |        \
+   [Процесс 1] [Процесс 2] [Процесс 3]
+      (ls)         (date)      (whoami)
